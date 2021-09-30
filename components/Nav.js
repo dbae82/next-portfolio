76d82a210cef63a57fb573eb7e39467f5b7e94d6
dbae82 <dbae82@gmail.com>
2021-09-30 12:04:02 -0500

    build: test projects page and adds logo and styles to nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,18 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
+import navStyles from '../styles/Nav.module.scss'
+import logo from '../assets/logo-dark.png'
 
 const Nav = () => {
     return (
         <nav>
-            <ul>
+            <ul className={navStyles.navComponents}>
                 <li>
-                    <Link href='/'>Home</Link>
+                    <Link href='/'><a><Image src={logo} alt='logo' /></a></Link>
+                </li>
+                <li>
+                    <Link href='projects'>Projects</Link>
                 </li>
                 <li>
                     <Link href='about'>About</Link>
