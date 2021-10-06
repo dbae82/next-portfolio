799c4cf0960cf692fe63958a10a52b5e58c473be
dbae82 <dbae82@gmail.com>
2021-10-06 16:25:16 -0500

    edit: adds project gif to sports tweets page

--- pages/projects/sports-tweets.js
+++ pages/projects/sports-tweets.js
@@ -1,6 +1,8 @@
+import Image from 'next/image'
 import Meta from '../../components/Meta'
 
 import projectPageStyles from '../../styles/projectPages.module.scss'
+import projectGif from '../../assets/sports-tweets.gif'
 
 const sportsTweets = () => {
     return (
@@ -14,6 +16,9 @@ const sportsTweets = () => {
                 <div className={projectPageStyles.col2}>
                     <p>Sports Tweets was my capstone project for my time at General Assembly. The idea came to me from a friend who wanted up to date news for his favorite sports. Powered by Twitter, I created Sports Tweets to stream the latest tweets depending on the user's choice of sport.</p>
                 </div>
+            </div>
+            <div className={projectPageStyles.projectGif}>
+                <Image src={projectGif} alt='sports-tweets' />
             </div>
         </>
     )
